c3f6c5a5680dcc492b21d046108a107bbd0a9d82
Diego Fernando Patzan Marroquin <dpatzan-2019173@kinal.edu.gt>
2022-10-27 17:11:42 -0600

    primero

--- src/LayoutGeneral.jsx
+++ src/LayoutGeneral.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 import { LoginPage } from './layouts/login/LoginPage';
+import { RegisterPage } from './layouts/register/RegisterPage';
 
 export const LayoutGeneral = () => {
   return (
     <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="login" element={<LoginPage />} />
-        <Route path="registro" element={<LoginPage />} />
+        <Route path="registro" element={<RegisterPage />} />
     </Routes>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { LayoutGeneral } from './LayoutGeneral';
 import reportWebVitals from './reportWebVitals';
+import './styles.general.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
