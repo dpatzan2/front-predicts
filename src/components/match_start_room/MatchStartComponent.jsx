d31c6285514a70ff5b0a461cf57d631318876391
JuanDsm <juandsolism@gmail.com>
2022-11-18 22:48:42 -0600

    Arreglos en Vista

--- src/components/match_start_room/MatchStartComponent.jsx
+++ src/components/match_start_room/MatchStartComponent.jsx
@@ -7,17 +7,21 @@ export const MatchStartComponent = ({data}) => {
     console.log(data, 'asdasdasd')
   return (
     <>
+    <div className={stylesMatchesCards.container__general__ojito}>
       <div className={stylesMatchesCards.container__general__matches} >
-        <div className={stylesMatchesCards.container__general__team__match}>
-          <div className={stylesMatchesCards.container__general__name__team}><img src={`https://drab-puce-puffer-sari.cyclic.app/${data.idTeam1.image}`} width="10" height="16" class="card-img-top" alt="..." />{data.idTeam1.name}</div>
-          {data.goalsTeam1}
+          <div className={stylesMatchesCards.container__general__team__match}>
+            <div className={stylesMatchesCards.container__general__name__team}><img src={`https://drab-puce-puffer-sari.cyclic.app/${data.idTeam1.image}`} width="10" height="16" class="card-img-top" alt="..." />{data.idTeam1.name}</div>
+            {data.goalsTeam1}
+          </div>
+          <div className={stylesMatchesCards.container__general__team__match}>
+            <div className={stylesMatchesCards.container__general__name__team}><img src={`https://drab-puce-puffer-sari.cyclic.app/${data.idTeam2.image}`} width="10" height="16" class="card-img-top" alt="..." />{data.idTeam2.name}</div>
+            {data.goalsTeam2}
+          </div>
         </div>
-        <div className={stylesMatchesCards.container__general__team__match}>
-          <div className={stylesMatchesCards.container__general__name__team}><img src={`https://drab-puce-puffer-sari.cyclic.app/${data.idTeam2.image}`} width="10" height="16" class="card-img-top" alt="..." />{data.idTeam2.name}</div>
-          {data.goalsTeam2}
+        <div className={stylesMatchesCards.container__general__team__time}>
+          <p className={stylesMatchesCards.container__general__team__time__text}>{moment(data.date).fromNow()}</p>
         </div>
-      </div>
-      <p>{moment(data.date).fromNow()}</p>
+    </div>
       
     </>
   );
